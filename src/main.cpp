291cb1f59a78641eac09ebde6ea36e5bce55e8af
Jonas M. Gastal <jgastal@gmail.com>
2008-12-09 23:21:44 +0300

    “” Fixed build configuration for release.

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	//Collection c("DefaultUser");
-	//std::cout << "Not yet implemented." << std::endl;
-	IniParser ini("config.ini");
-
-	string section;
-	while((section = ini.getNextSection()) != "")
-	{
-		cout << "CurrentSection: " << section << endl;
-		cout << "field0 = " << ini.getValue("field0") << endl;
-		cout << "field1 = " << ini.getValue("field1") << endl;
-	}
+	Collection c("DefaultUser");
+	std::cout << "Not yet implemented." << std::endl;
 }
