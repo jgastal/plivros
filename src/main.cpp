8a95d7a66c0b0f5897d39df9de15c893aecaa766
Jonas M. Gastal <jgastal@gmail.com>
2008-12-09 22:42:15 +0300

    “” Initial implementation of ini parser.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
 #include "Collection.h"
+#include "IniParser.h"
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
-	Collection c("DefaultUser");
-	std::cout << "Not yet implemented." << std::endl;
+	//Collection c("DefaultUser");
+	//std::cout << "Not yet implemented." << std::endl;
+	IniParser ini("config.ini");
+
+	string section;
+	while((section = ini.getNextSection()) != "")
+	{
+		cout << "CurrentSection: " << section << endl;
+		cout << "field0 = " << ini.getValue("field0") << endl;
+		cout << "field1 = " << ini.getValue("field1") << endl;
+	}
 }
