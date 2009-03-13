3e562462226a02ea6703603c659be360783be440
Jonas M. Gastal <jgastal@gmail.com>
2009-03-13 03:34:14 +0300

    “” Started implementing GUI(completely removed text interface). Created a rough draft ui of the main window, and corresponding translation. CMakeLists.txt for ui and translations currently don't work, for unknown reason.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <QMainWindow>
+#include <QTranslator>
 
 #include "Collection.h"
 #include "Book.h"
@@ -6,171 +7,30 @@
 #include "Publisher.h"
 #include "Theme.h"
 
-using namespace std;
+#include "ui_MainWindow.h"
 
-void displayBooks(QList<Book> &blist);
-void displayAuthors(QList<Author> &alist);
-void displayPublishers(QList<Publisher> &plist);
-void displayThemes(QList<Theme> &tlist);
-void displayBook(Book &b);
-void displayAuthor(Author &a);
-void displayPublisher(Publisher &p);
-void displayTheme(Theme &t);
+class Window : public QMainWindow, private Ui::MainWindow
+{
+	public:
+		Window(QMainWindow *parent = 0);
+} ;
+
+Window::Window(QMainWindow *parent) : QMainWindow(parent)
+{
+	setupUi(this);
+}
 
 int main(int argc, char **argv)
 {
-	try
-	{
-		Collection c("User");
+	QApplication app(argc, argv);
+	QTranslator translator;
 
-		QList<Theme> themeList;
-		QList<Author> authorList;
-		QList<Publisher> publisherList;
+	//translate to currently used locale
+	QString locale = QLocale::system().name();
+	translator.load(QString("plivros_") + locale);
+	app.installTranslator(&translator);
 
-		Theme t("theme1", "desc1");
-		c.insertTheme(t);
-		themeList.append(t);
-
-		Publisher p("pub1", "desc", "cri", "lg", themeList);
-		c.insertPublisher(p);
-		publisherList.append(p);
-
-		Author a("fn", "ln", "desc", "cri", 5, "pic", themeList);
-		c.insertAuthor(a);
-		authorList.append(a);
-
-		Book b("0123456789123", "title", 1, "cri", "desc", 5, "cover", "ebook", QDate(2009,1,9),
-			"UDC", authorList, a, publisherList, themeList);
-
-		c.insertBook(b);
-
-		cout << endl << "Searching books by description: " << endl << endl;
-		
-		QList<Book> bookList = c.searchBooks(Book::b_description, "sc");
-		displayBooks(bookList);
-
-		cout << endl << "Searching books by translator: " << endl << endl;
-
-		bookList = c.searchBooks(Book::b_translator, "ln");
-		displayBooks(bookList);
-
-		cout << endl << "Searching authors by theme: " << endl << endl;
-
-		authorList = c.searchAuthors(Author::a_themes, "theme");
-		c.searchAuthors(Author::a_themes, "theme");
-		displayAuthors(authorList);
-
-		cout << endl << "Searching publishers by theme: " << endl << endl;
-
-		publisherList = c.searchPublishers(Publisher::p_themes, "theme");
-		c.searchPublishers(Publisher::p_themes, "theme");
-		displayPublishers(publisherList);
-	}
-	catch(bad_alloc e)
-	{
-		cout << "Not enough memory." << endl;
-	}
-	catch(out_of_range e)
-	{
-		cout << "Out of range error." << endl;
-		cout << e.what() << endl;
-	}
-	catch(DataBaseException dbe)
-	{
-		cout << "DataBase error: " << dbe.what() << endl;
-		cout << "Cause: " << dbe.where() << endl;
-		cout << "Error code: " << dbe.getCode() << endl;
-	}
-}
-
-void displayBooks(QList<Book> &blist)
-{
-	QList<Book>::iterator it;
-	for(it = blist.begin(); it != blist.end(); it++)
-		displayBook((*it));
-}
-
-void displayAuthors(QList<Author> &alist)
-{
-	QList<Author>::iterator it;
-	for(it = alist.begin(); it != alist.end(); it++)
-		displayAuthor((*it));
-}
-
-void displayPublishers(QList<Publisher> &plist)
-{
-	QList<Publisher>::iterator it;
-	for(it = plist.begin(); it !=plist.end(); it++)
-		displayPublisher((*it));
-}
-
-void displayThemes(QList<Theme> &tlist)
-{
-	QList<Theme>::iterator it;
-	for(it = tlist.begin(); it != tlist.end(); it++)
-		displayTheme((*it));
-}
-
-void displayBook(Book &b)
-{
-	cout << "--- Begin Book ---" << endl;
-	cout << "Title: " << b.getTitle() << endl;
-	cout << "ISBN: " << b.getIsbn() << endl;
-	cout << "Edition: " << b.getEdition() << endl;
-	cout << "Description: " << b.getDescription() << endl;
-	cout << "Critique: " << b.getCritique() << endl;
-	cout << "Rating: " << b.getRating() << endl;
-	cout << "Cover: " << b.getCover() << endl;
-	cout << "Ebook: " << b.getEbook() << endl;
-	cout << "Publication Date: " << b.getPubDate().toString().toStdString() << endl;
-	cout << "UDC: " << b.getUDC() << endl;
-	cout << "Authors: " << endl;
-	QList<Author> aList = b.getAuthors();
-	displayAuthors(aList);
-	cout << "Translator: " << endl;
-	Author a = b.getTranslator();
-	displayAuthor(a);
-	cout << "Publishers: " << endl;
-	QList<Publisher> pList = b.getPublishers();
-	displayPublishers(pList);
-	cout << "Themes: " << endl;
-	QList<Theme> tList = b.getThemes();
-	displayThemes(tList);
-	cout << "--- End Book ---" << endl;
-}
-
-void displayAuthor(Author &a)
-{
-	cout << "--- Begin Author ---" << endl;
-	cout << "First name: " << a.getFirstName() << endl;
-	cout << "Last name: " << a.getLastName() << endl;
-	cout << "Description: " << a.getDescription() << endl;
-	cout << "Critique: " << a.getCritique() << endl;
-	cout << "Rating: " << a.getRating() << endl;
-	cout << "Picture: " << a.getPicture() << endl;
-	cout << "Themes: " << endl;
-	QList<Theme> tList = a.getThemes();
-	displayThemes(tList);
-	cout << "--- End Author ---" << endl;
-}
-
-void displayPublisher(Publisher &p)
-{
-	cout << "--- Begin Publisher ---" << endl;
-	cout << "Name: " << p.getName() << endl;
-	cout << "Description: " << p.getDescription() << endl;
-	cout << "Critique: " << p.getCritique() << endl;
-	cout << "Logo: " << p.getLogo() << endl;
-	cout << "Themes: " << endl;
-	QList<Theme> tList = p.getThemes();
-	displayThemes(tList);
-	cout << "--- End Publisher ---" << endl;
-}
-
-void displayTheme(Theme &t)
-{
-	cout << "--- Begin Theme ---" << endl;
-	cout << "Name: " << t.getName() << endl;
-	cout << "Description: " << t.getDescription() << endl;
-	cout << "--- End Theme ---" << endl;
+	Window w;
+	w.show();
+	return app.exec();
 }
---
+++ ts/mainwindow_pt_BR.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Janela Principal</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="45"/>
+        <source>Books</source>
+        <translation>Livros</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="66"/>
+        <location filename="../UI/MainWindow.ui" line="141"/>
+        <location filename="../UI/MainWindow.ui" line="216"/>
+        <location filename="../UI/MainWindow.ui" line="291"/>
+        <source>Add</source>
+        <translation>Adicionar</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="79"/>
+        <location filename="../UI/MainWindow.ui" line="154"/>
+        <location filename="../UI/MainWindow.ui" line="229"/>
+        <location filename="../UI/MainWindow.ui" line="304"/>
+        <source>Search</source>
+        <translation>Procurar</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="92"/>
+        <location filename="../UI/MainWindow.ui" line="167"/>
+        <location filename="../UI/MainWindow.ui" line="242"/>
+        <location filename="../UI/MainWindow.ui" line="317"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="105"/>
+        <location filename="../UI/MainWindow.ui" line="180"/>
+        <location filename="../UI/MainWindow.ui" line="255"/>
+        <location filename="../UI/MainWindow.ui" line="330"/>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="120"/>
+        <source>Authors</source>
+        <translation>Autores</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="195"/>
+        <source>Publishers</source>
+        <translation>Editoras</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="270"/>
+        <source>Themes</source>
+        <translation>Temas</translation>
+    </message>
+    <message>
+        <location filename="../UI/MainWindow.ui" line="344"/>
+        <source>Tab 1</source>
+        <translation>Aba 1</translation>
+    </message>
+</context>
+</TS>
