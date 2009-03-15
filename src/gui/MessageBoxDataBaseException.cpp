b5be394ed74841da47b276ff0c84d7fb760f5feb
Jonas M. Gastal <jgastal@gmail.com>
2009-03-15 22:12:10 +0300

    “” Added documentation on MessageBoxDataBaseException class.

--- src/gui/MessageBoxDataBaseException.cpp
+++ src/gui/MessageBoxDataBaseException.cpp
@@ -1,5 +1,21 @@
+/**
+ * @file MessageBoxDataBaseException.cpp
+ *
+ * @date 15/03/2009
+ * @author Jonas M. Gastal
+ * @brief Implements MessageBoxDataBaseException class.
+ */
+
 #include "MessageBoxDataBaseException.h"
 
+/**
+ * @brief Constructor
+ *
+ * @param parent Parent of this widget passed on to QMessageBox constructor.
+ *
+ * This constructor in addition to constructing the object sets its icon,
+ * text, default button and detailed text.
+ */
 MessageBoxDataBaseException::MessageBoxDataBaseException(QWidget *parent) : QMessageBox(parent)
 {
 	setIcon(QMessageBox::Critical);
@@ -12,6 +28,11 @@ MessageBoxDataBaseException::MessageBoxDataBaseException(QWidget *parent) : QMes
 	setDetailedText(str);
 }
 
+/**
+ * @brief Appends \a str to the current text of this object.
+ *
+ * @param str Text to be appended to current text.
+ */
 void MessageBoxDataBaseException::appendText(QString str)
 {
 	QString str = text();
--- src/gui/MessageBoxDataBaseException.h
+++ src/gui/MessageBoxDataBaseException.h
@@ -1,8 +1,27 @@
+/**
+ * @file MessageBoxDataBaseException.h
+ *
+ * @date 15/03/2009
+ * @author Jonas M. Gastal
+ * @brief Defines MessageBoxDataBaseException class.
+ */
+
 #ifndef MESSAGEBOXDATABASEEXCEPTION_H
 #define MESSAGEBOXDATABASEEXCEPTION_H
 
 #include <QMessageBox>
 
+/**
+ * @class MessageBoxDataBaseException MessageBoxDataBaseException.h
+ *
+ * @brief This class is a wrapper around QMessageBox to display DataBaseExceptions.
+ *
+ * This class inherits from QMessageBox. It was created as a convenient way to
+ * write less code, it sets a number of presets to the QMessageBox that are good
+ * for DataBaseExceptions, such as: Icon, Detailed text, Default button and a text
+ * that says the error was unexpected and serious. Also provided for convenience
+ * is a method to append to text.
+ */
 class MessageBoxDataBaseException : public QMessageBox
 {
 	public:
