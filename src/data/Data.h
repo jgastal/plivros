964b517e30edcdfd16baffb70c96f5ead3ad46b6
Jonas M. Gastal <jokerwww@gmail.com>
2009-03-20 23:57:49 +0300

    Since a lot of files include all of data created a meta include for it.

---
+++ src/data/Data.h
@@ -0,0 +1,17 @@
+/**
+ * @file Data.h
+ *
+ * @date 20/03/2009
+ * @author Jonas M. Gastal
+ * @brief Meta include for all includes in data.
+ */
+
+#ifndef DATA_H
+#define DATA_H
+
+#include "Book.h"
+#include "Author.h"
+#include "Publisher.h"
+#include "Theme.h"
+
+#endif //DATA_H
